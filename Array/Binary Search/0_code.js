ff0e19287722056d395063ec57d3086565f9e1c1
RutvikMakvana4 <rutvikmakvana432@gmail.com>
2024-10-25 21:48:22 +0530

    feat: add binary search topic

---
+++ Array/Binary Search/0_code.js
@@ -0,0 +1,29 @@
+/*
+ * Binary Search
+ */
+function binarySearch(arr, key) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  let mid = Math.ceil((start + end) / 2);
+
+  while (start <= end) {
+    if (arr[mid] == key) {
+      return mid;
+    }
+
+    if (key > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+
+    mid = Math.ceil((start + end) / 2);
+  }
+
+  return -1;
+}
+
+const arr = [2, 4, 6, 8, 10, 12];
+const key = 10;
+console.log(binarySearch(arr, key));
---
+++ Array/Binary Search/readme.md
@@ -0,0 +1,30 @@
+## Binary Search
+
+- Its higher version of Linear search
+
+```sh
+condition :- array should be aecending or decending order
+```
+
+```sh
+
+const arr = [2, 4, 7, 11, 13, 17]
+const key = 13
+
+
+mid = (start index + end index) / 2
+
+mid = 0 + 5/ 2 = 5/2 = 2.5 = 2
+mid = 7
+
+
+
+mid == key
+7 == 13
+NO....
+
+key > mid
+
+key < mid
+
+```
